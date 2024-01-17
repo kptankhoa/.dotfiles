06f9e4b444c05413f17afd09e2b70f5b8a9931d6
kptankhoa <kptankhoa@gmail.com>
2024-01-17 10:58:19 +0700

    add down ssh

---
+++ script/down-ssh.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DF_HOME=$HOME/.dotfiles
+
+if [ ! -d $DF_HOME/.ssh ]; then
+  exit 1
+fi
+
+rm $HOME/.ssh/*
+
+for FILE in $DF_HOME/.ssh/*;
+do
+    if [ ! -f "$FILE" ]; then
+        touch $HOME/.ssh/$(basename $FILE)
+    fi
+    ln -s $FILE $HOME/.ssh/$(basename $FILE)
+done
+
