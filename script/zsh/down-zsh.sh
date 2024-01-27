09e4a78154da64caa7e1b9129d6a2d28afba79d9
kptankhoa <kptankhoa@gmail.com>
2024-01-27 22:32:41 +0700

    update

--- script/zsh/down-zsh.sh
+++ script/zsh/down-zsh.sh
@@ -7,22 +7,7 @@ if [ -f $DF_HOME/.zshrc ]; then
     ln -s $DF_HOME/.zshrc $HOME/.zshrc
 fi
 
-if [ -f $DF_HOME/.p10k.zsh ]; then
-    touch $HOME/.p10k.zsh
-    ln -s $DF_HOME/.p10k.zsh $HOME/.p10k.zsh
-fi
-
 if [ -f $DF_HOME/.zsh_aliases ]; then
     touch $HOME/.zsh_aliases
     ln -s $DF_HOME/.zsh_aliases $HOME/.zsh_aliases
 fi
-
-if [ -d $HOME/.oh-my-zsh ]; then
-    rm -rf $HOME/.oh-my-zsh
-fi
-cp -r $DF_HOME/.oh-my-zsh $DF_HOME/.oh-my-zsh
-
-if [ -d $HOME/powerlevel10k ]; then
-    rm -rf $HOME/powerlevel10k
-fi
-cp -r $DF_HOME/powerlevel10k $HOME/powerlevel10k
--- script/zsh/up-zsh.sh
+++ script/zsh/up-zsh.sh
@@ -11,16 +11,3 @@ if [ -f $HOME/.zsh_aliases ]; then
     mv $HOME/.zsh_aliases $DF_HOME/.zsh_aliases
     ln -s $DF_HOME/.zsh_aliases $HOME/.zsh_aliases
 fi
-
-if [ -f $HOME/.p10k.zsh ]; then
-    mv $HOME/.p10k.zsh $DF_HOME/.p10k.zsh
-    ln -s $DF_HOME/.p10k.zsh $HOME/.p10k.zsh
-fi
-
-if [ -d $HOME/.oh-my-zsh ]; then
-    cp -r $HOME/.oh-my-zsh $DF_HOME/.oh-my-zsh
-fi
-
-if [ -d $HOME/powerlevel10k ]; then
-    cp -r $HOME/powerlevel10k $DF_HOME/powerlevel10k
-fi
