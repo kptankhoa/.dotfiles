77c44f36a76a461f4a4b0604dc41a4a7049285b2
kptankhoa <khoa.phan@codelink.io>
2024-08-18 01:39:40 +0700

    update wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,16 +1,12 @@
--- Pull in the wezterm API
 local wezterm = require 'wezterm'
 
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
 config.hide_tab_bar_if_only_one_tab = true
-config.use_fancy_tab_bar = true
+config.font_size = 10
 
+config.window_decorations = "RESIZE"
 
--- For example, changing the color scheme:
 config.color_scheme = 'Catppuccin Mocha'
 
--- and finally, return the configuration to wezterm
 return config
