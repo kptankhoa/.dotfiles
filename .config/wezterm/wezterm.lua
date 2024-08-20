78fb072f25f09ad0be018fb9e4c844bf58bf408c
kptankhoa <kptankhoa@gmail.com>
2024-08-20 23:49:19 +0700

    change style to gruvbox

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,12 +1,13 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
 config.hide_tab_bar_if_only_one_tab = true
-config.font_size = 10
+
+config.font_size = 9
 
 config.window_decorations = "RESIZE"
 
-config.color_scheme = 'Catppuccin Mocha'
+config.color_scheme = "Gruvbox Dark (Gogh)"
 
 return config
