13f17226857ef670604bad6497acb4a04abab3c5
kptankhoa <kptankhoa@gmail.com>
2024-08-22 02:37:46 +0700

    update gruvbox

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -1,14 +1,25 @@
 return {
+  --  {
+  --    "ellisonleao/gruvbox.nvim",
+  --    opts = {
+  --      contrast = "dark",
+  --    },
+  --  },
+  --  {
+  --    "LazyVim/LazyVim",
+  --    opts = {
+  --      colorscheme = "gruvbox",
+  --    },
+  --  },
   {
-    "ellisonleao/gruvbox.nvim",
-    opts = {
-      contrast = "dark",
-    },
-  },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "gruvbox",
-    },
+    "sainnhe/gruvbox-material",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- Optionally configure and load the colorscheme
+      -- directly inside the plugin declaration.
+      vim.g.gruvbox_material_enable_italic = true
+      vim.cmd.colorscheme("gruvbox-material")
+    end,
   },
 }
